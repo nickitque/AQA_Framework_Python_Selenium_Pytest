f6609d686f48d1c38c47c74df468a144c8ca2674
Nikita Radzisheuski <nikitaradzisheuski@nikitas-macbook-pro.home>
2023-11-04 10:47:07 +0100

    Added new tests and created docstrings

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -3,13 +3,15 @@ from pages.home_page import HomePage
 link = "https://takservice.pl/ru/"
 
 
-@pytest.mark.skip(reason=None)
+# @pytest.mark.skip(reason=None):
 def test_guest_can_visit_home_page(browser):
+    """Opening the page."""
     page = HomePage(browser, link)
     page.open()
 
 
 def test_guest_should_see_address_text(browser):
+    """Validating address text in header."""
     page = HomePage(browser, link)  # initialize the Object, pass a driver instance to the constructor
     page.open()
     page.should_be_address_text()
@@ -17,12 +19,14 @@ def test_guest_should_see_address_text(browser):
 
 
 def test_guest_should_see_address_icon(browser):
+    """Check the presence of icon."""
     page = HomePage(browser, link)
     page.open()
     page.should_be_address_icon()
 
 
 def test_guest_should_see_phone_text(browser):
+    """Check the presence of phone number text and validation."""
     page = HomePage(browser, link)  # initialize the Object, pass a driver instance to the constructor
     page.open()
     page.should_be_phone_text()
@@ -30,18 +34,21 @@ def test_guest_should_see_phone_text(browser):
 
 
 def test_guest_should_see_phone_icon(browser):
+    """Check the presence of icon."""
     page = HomePage(browser, link)
     page.open()
     page.should_be_phone_icon()
 
 
 def test_guest_should_see_whatsapp_icon(browser):
+    """Check the presence of icon."""
     page = HomePage(browser, link)
     page.open()
     page.check_whatsapp_link_header()
 
 
 def test_guest_can_click_logo_in_header(browser):
+    """Clicking the icon in header and validating browser url after click."""
     page = HomePage(browser, link)
     page.open()
     page.click_logo_header()
@@ -49,30 +56,35 @@ def test_guest_can_click_logo_in_header(browser):
 
 
 def test_guest_can_click_services_in_header(browser):
+    """Clicking the services btn and validating the url (the url should have anchor)."""
     page = HomePage(browser, link)
     page.open()
     page.click_services_header()
 
 
 def test_guest_can_click_about_us_in_header(browser):
+    """Clicking the services btn and validating the url (the url should have anchor)."""
     page = HomePage(browser, link)
     page.open()
     page.click_about_us_header()
 
 
 def test_guest_can_click_reviews_in_header(browser):
+    """Clicking the services btn and validating the url (the url should have anchor)."""
     page = HomePage(browser, link)
     page.open()
     page.click_reviews_header()
 
 
 def test_guest_can_click_contacts_in_header(browser):
+    """Clicking the services btn and validating the url (the url should have anchor)."""
     page = HomePage(browser, link)
     page.open()
     page.click_contacts_header()
 
 
 def test_guest_can_click_call_me_back_and_send_valid_data_to_cf(browser):
+    """Clicking the call me back btn, filling and sending CF in Pop-up."""
     page = HomePage(browser, link)
     page.open()
     page.click_call_back_header()
@@ -80,10 +92,12 @@ def test_guest_can_click_call_me_back_and_send_valid_data_to_cf(browser):
 
 
 def test_guest_can_click_signup_btn_and_send_cf(browser):
+    """Clicking the call me back btn, filling and sending CF."""
     page = HomePage(browser, link)
     page.open()
     page.click_sign_up_button()
     # page.fill_contact_form_homepage()
+
 
 def test_guest_can_click_first_promotion_card(browser):
     page = HomePage(browser, link)
--- tests/test_promotion_page.py
+++ tests/test_promotion_page.py
@@ -27,3 +27,11 @@ def test_guest_can_visit_home_page_from_fourth_endpoint(browser):
     page = PromotionsPage(browser, link + endpoints[3])
     page.open()
     page.click_back_to_homepage_button()
+
+
+def test_guest_can_click_call_back_button_and_fill_cf(browser):
+    page = PromotionsPage(browser, link + endpoints[0])
+    page.open()
+    page.click_call_back_header()
+    page.fill_popup_contact_form_valid_data()
+    #needs update because of the selectors
