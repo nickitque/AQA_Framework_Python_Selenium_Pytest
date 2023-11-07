5c04f8e7dc5de2377b2f86818997a2113627bd85
Nikita Radzisheuski <nikitaradzisheuski@nikitas-macbook-pro.home>
2023-11-07 16:13:20 +0100

    Deleted unnecessary file

--- locustfile.py
+++
@@ -1,7 +0,0 @@
-from locust import HttpUser, task
-
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("new-casinos")
-        self.client.get("bitcoin")
